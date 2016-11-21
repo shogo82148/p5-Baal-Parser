0973b6a8bac46679c1a6f6816ab2c0b418729d7b
ohta-nobuyuki <ohta-nobuyuki@kayac.com>
2016-11-21 11:04:14 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Baal::Parser - It's new $module
+
+# SYNOPSIS
+
+    use Baal::Parser;
+
+# DESCRIPTION
+
+Baal::Parser is ...
+
+# LICENSE
+
+Copyright (C) ohta-nobuyuki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ohta-nobuyuki <ohta-nobuyuki@kayac.com>
---
+++ lib/Baal/Parser.pm
@@ -0,0 +1,39 @@
+package Baal::Parser;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Baal::Parser - It's new $module
+
+=head1 SYNOPSIS
+
+    use Baal::Parser;
+
+=head1 DESCRIPTION
+
+Baal::Parser is ...
+
+=head1 LICENSE
+
+Copyright (C) ohta-nobuyuki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ohta-nobuyuki E<lt>ohta-nobuyuki@kayac.comE<gt>
+
+=cut
+
